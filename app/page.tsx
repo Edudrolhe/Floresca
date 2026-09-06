01e2c9c31e73b2e2308d259c36f3044e15871c93
Edudrolhe <edudrolhe@gmail.com>
2026-09-06 09:25:13 -0300

    chore: add shadcn

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,7 @@
+import { Button } from "./_components/ui/button";
+
 export default function Home() {
-  return <h1>Home page</h1>
+  return <Button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+    Hello, World!
+  </Button>
 }
